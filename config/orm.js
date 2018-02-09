066fe1bca473efa44f99f4dcbef3c4fa992f080e
ohsnaplol <emyb@bellsouth.net>
2018-02-09 14:40:34 -0500

    added orm

--- config/orm.js
+++ config/orm.js
@@ -1,19 +1,41 @@
 var connection = require('./connection.js')
 
 var orm = {
-    selectAll: function() {
+    selectAll: function(from) {
         return new Promise(function(resolve, reject) {
-            connection.query('SELECT * FROM ??')
+            connection.query('SELECT * FROM ??', [from],function(error, result) {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
         })
     },
-    insertOne: function() {
+    insertOne: function(burger_name, devoured, date) {
+        var queryString = 'INSERT INTO burgers(burger_name, devoured, date) VALUES(??, ??, ??)'
         return new Promise(function(resolve, reject) {
-            connection.query('INSERT INTO burgers(burger_name, devoured, date) VALUES(??, ??, ??)')
+            connection.query(queryString,[burger_name, devoured, date], function(error, result) {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
         })
     },
-    updateOne: function() {
+    updateOne: function(whatToSet, setToWhat, where, is) {
+        var queryString = 'UPDATE burgers SET ?? = ?? WHERE ?? = ??'
         return new Promise(function(resolve, reject) {
-            connection.query() 
+            connection.query(queryString,[whatToSet, setToWhat, where, is], function(error, result) {
+                if (error) {
+                    reject(error)
+                } else {
+                    resolve(result)
+                }
+            })
         })
     }
 }
+
+module.exports = orm
