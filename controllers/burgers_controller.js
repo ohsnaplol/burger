1b27951dcefb7b992a0a07a72bc3af6530a44250
ohsnaplol <emyb@bellsouth.net>
2018-02-12 15:32:17 -0500

    added burger controller

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,23 @@
+var express = require('express.js')
+var burger = require('../models/burger.js')
+var router = express.Router()
+
+router.get('/', function(request, response) {
+  burger.getTable(function(data) {
+    burger.getTable(function(data) {
+      response.render('index', { cats: data })
+    })
+  })
+})
+
+router.post('/api/cats', function(request, response) {
+  burger.add(request.body.name)
+  response.json({response})
+})
+
+router.put('api/cats/:id', function(request,response) {
+  burger.devour(request.params.id)
+  // response.json({response})
+})
+
+modules.exports = router
