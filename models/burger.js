8dfa9044135084c69c23eddbe27f6f9c8e4fee65
ohsnaplol <emyb@bellsouth.net>
2018-02-13 12:11:49 -0500

    added .then to orm calls

--- models/burger.js
+++ models/burger.js
@@ -2,15 +2,30 @@ var orm = require('../config/orm.js')
 
 var burger = {
   getTable: function() {
-    orm.selectAll('burgers')
+    orm.selectAll('burgers').then(function(result) {
+      console.log(result)
+      return result
+    }, function(err) {
+      console.log(err)
+    })
   },
   // by default, is not devoured
   add: function(name) {
-    orm.insertOne(name, 0)
+    orm.insertOne(name, 0).then(function(result) {
+      console.log(result)
+      return result
+    }, function(err) {
+      console.log(err)
+    })
   },
   // Set devoured to true at burger with id
   devour: function(id) {
-    orm.updateOne('devoured', 1, 'id', id)
+    orm.updateOne('devoured', 1, 'id', id).then(function(result) {
+      console.log(result)
+      return result
+    }, function(err) {
+      console.log(err)
+    })
   }
 }
 
